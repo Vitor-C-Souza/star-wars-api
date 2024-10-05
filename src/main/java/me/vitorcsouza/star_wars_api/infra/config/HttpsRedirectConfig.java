3b3fbfeedf9cd06e9f144e52ebb9fd5101f1ebf9
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-10-04 23:20:58 -0300

    Reajustando o Cors para o deploy da API

---
+++ src/main/java/me/vitorcsouza/star_wars_api/infra/config/HttpsRedirectConfig.java
@@ -0,0 +1,26 @@
+package me.vitorcsouza.star_wars_api.infra.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsRedirectConfig {
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainer() {
+        return server -> {
+            server.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+        };
+    }
+
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);  // Porta HTTP
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);  // Porta HTTPS
+        return connector;
+    }
+}
--- src/main/java/me/vitorcsouza/star_wars_api/infra/config/WebConfig.java
+++ src/main/java/me/vitorcsouza/star_wars_api/infra/config/WebConfig.java
@@ -1,4 +1,4 @@
-package me.vitorcsouza.star_wars_api;
+package me.vitorcsouza.star_wars_api.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
 }
